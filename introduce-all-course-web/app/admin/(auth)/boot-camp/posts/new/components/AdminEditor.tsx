3b2ab3f5c9ce5de0d6db8f734839c814f1817b33
YugyeomKim <hoodie0512@gmail.com>
2024-07-25 14:29:12 +0900

    Design: Make AdminEditor scrollable

--- introduce-all-course-web/app/admin/(auth)/boot-camp/posts/new/components/AdminEditor.tsx
+++ introduce-all-course-web/app/admin/(auth)/boot-camp/posts/new/components/AdminEditor.tsx
@@ -29,6 +29,7 @@ import {
   AdminSelectTrigger,
   AdminSelectValue,
 } from "@/app/admin/components/ui/admin-select";
+import { Separator } from "@/components/ui/separator";
 
 const AdminEditor = () => {
   const [selectedHeading, setSelectedHeading] = useState("T");
@@ -112,154 +113,170 @@ const AdminEditor = () => {
   });
 
   return (
-    <div className="group rounded-lg border border-gray-300 focus-within:border-black">
-      <div className="m-1 flex flex-row items-center space-x-1">
-        <AdminSelect
-          value={
-            editor.isActive("heading", { level: 1 })
-              ? "H1"
-              : editor.isActive("heading", { level: 2 })
-              ? "H2"
-              : editor.isActive("heading", { level: 3 })
-              ? "H3"
-              : "T"
-          }
-          onValueChange={handleHeadingChange}
-        >
-          <AdminSelectTrigger
-            className={cn(
-              adminButtonVariants({ size: "icon", variant: "ghost" }),
-              "h-8 w-8 border-0 "
-            )}
-          >
-            <AdminSelectValue>{selectedHeading}</AdminSelectValue>
-          </AdminSelectTrigger>
-          <AdminSelectContent>
-            <AdminSelectGroup>
-              <AdminSelectItem value="H1">대제목</AdminSelectItem>
-              <AdminSelectItem value="H2">중제목</AdminSelectItem>
-              <AdminSelectItem value="H3">소제목</AdminSelectItem>
-              <AdminSelectItem value="T">본문</AdminSelectItem>
-            </AdminSelectGroup>
-          </AdminSelectContent>
-        </AdminSelect>
-        <AdminButton
-          onClick={() => editor.chain().focus().toggleBold().run()}
-          variant="ghost"
-          size="icon"
-          className={cn(
-            "h-8 w-8",
-            editor.isActive("bold") && "bg-accent text-accent-foreground"
-          )}
-        >
-          <strong>B</strong>
-        </AdminButton>
-        <AdminButton
-          onClick={() => editor.chain().focus().toggleItalic().run()}
-          variant="ghost"
-          size="icon"
-          className={cn(
-            "h-8 w-8",
-            editor.isActive("italic") && "bg-accent text-accent-foreground"
-          )}
-        >
-          <i>i</i>
-        </AdminButton>
-        <AdminButton
-          onClick={() => editor.chain().focus().toggleStrike().run()}
-          variant="ghost"
-          size="icon"
-          className={cn(
-            "h-8 w-8",
-            editor.isActive("strike") && "bg-accent text-accent-foreground"
-          )}
-        >
-          <StrikethroughIcon className="h-4 w-4" />
-        </AdminButton>
-        <AdminButton
-          onClick={() => editor.chain().focus().toggleHighlight().run()}
-          variant="ghost"
-          size="icon"
-          className={cn(
-            "h-8 w-8",
-            editor.isActive("highlight") && "bg-accent text-accent-foreground"
-          )}
-        >
-          <span className="w-4 bg-yellow-200">H</span>
-        </AdminButton>
-        <AdminButton
-          onClick={() => editor.chain().focus().toggleUnderline().run()}
-          variant="ghost"
-          size="icon"
-          className={cn(
-            "h-8 w-8",
-            editor.isActive("underline") && "bg-accent text-accent-foreground"
-          )}
-        >
-          <span className="w-4 underline">U</span>
-        </AdminButton>
-        <div className="h-5 w-px bg-gray-300" />
-        <AdminButton
-          onClick={() => editor.chain().focus().toggleBulletList().run()}
-          variant="ghost"
-          size="icon"
-          className={cn(
-            "h-8 w-8",
-            editor.isActive("bulletList") && "bg-accent text-accent-foreground"
-          )}
-        >
-          <ListIcon className="h-4 w-4" />
-        </AdminButton>
-        <AdminButton
-          onClick={() => editor.chain().focus().toggleOrderedList().run()}
-          variant="ghost"
-          size="icon"
-          className={cn(
-            "h-8 w-8",
-            editor.isActive("orderedList") && "bg-accent text-accent-foreground"
-          )}
-        >
-          <ListOrderedIcon className="h-4 w-4" />
-        </AdminButton>
-        <AdminButton
-          onClick={() => editor.chain().focus().toggleBlockquote().run()}
-          variant="ghost"
-          size="icon"
-          className={cn(
-            "h-8 w-8",
-            editor.isActive("blockquote") && "bg-accent text-accent-foreground"
-          )}
-        >
-          <QuoteIcon className="h-4 w-4" />
-        </AdminButton>
-        <div className="h-5 w-px bg-gray-300" />
-        <AdminButton
-          onClick={() => {}}
-          variant="ghost"
-          size="icon"
-          className="h-8 w-8"
-        >
-          <ImageIcon className="h-4 w-4" />
-        </AdminButton>
-        <div className="h-5 w-px bg-gray-300" />
-        <AdminButton
-          onClick={() => editor.chain().focus().undo().run()}
-          disabled={!editor.can().undo()}
-          variant="ghost"
-          size="icon"
-          className="h-8 w-8"
-        >
-          <Undo2Icon className="h-4 w-4" />
-        </AdminButton>
-        <AdminButton
-          onClick={() => editor.chain().focus().redo().run()}
-          disabled={!editor.can().redo()}
-          variant="ghost"
-          size="icon"
-          className="h-8 w-8"
-        >
-          <Redo2Icon className="h-4 w-4" />
-        </AdminButton>
+    <div className="hidden-scrollbar group max-h-[90vh] space-y-1 overflow-y-scroll rounded-lg border border-gray-300 focus-within:border-black">
+      <div className="sticky top-0 z-10 space-y-1 bg-white px-1 pt-1">
+        <div className="flex flex-row items-center justify-between">
+          <div className="flex flex-row items-center space-x-1">
+            <AdminSelect
+              value={
+                editor.isActive("heading", { level: 1 })
+                  ? "H1"
+                  : editor.isActive("heading", { level: 2 })
+                  ? "H2"
+                  : editor.isActive("heading", { level: 3 })
+                  ? "H3"
+                  : "T"
+              }
+              onValueChange={handleHeadingChange}
+            >
+              <AdminSelectTrigger
+                className={cn(
+                  adminButtonVariants({ size: "icon", variant: "ghost" }),
+                  "h-8 w-8 border-0 "
+                )}
+              >
+                <AdminSelectValue>{selectedHeading}</AdminSelectValue>
+              </AdminSelectTrigger>
+              <AdminSelectContent>
+                <AdminSelectGroup>
+                  <AdminSelectItem value="H1">대제목</AdminSelectItem>
+                  <AdminSelectItem value="H2">중제목</AdminSelectItem>
+                  <AdminSelectItem value="H3">소제목</AdminSelectItem>
+                  <AdminSelectItem value="T">본문</AdminSelectItem>
+                </AdminSelectGroup>
+              </AdminSelectContent>
+            </AdminSelect>
+            <AdminButton
+              onClick={() => editor.chain().focus().toggleBold().run()}
+              variant="ghost"
+              size="icon"
+              className={cn(
+                "h-8 w-8",
+                editor.isActive("bold") && "bg-accent text-accent-foreground"
+              )}
+            >
+              <strong>B</strong>
+            </AdminButton>
+            <AdminButton
+              onClick={() => editor.chain().focus().toggleItalic().run()}
+              variant="ghost"
+              size="icon"
+              className={cn(
+                "h-8 w-8",
+                editor.isActive("italic") && "bg-accent text-accent-foreground"
+              )}
+            >
+              <i>i</i>
+            </AdminButton>
+            <AdminButton
+              onClick={() => editor.chain().focus().toggleStrike().run()}
+              variant="ghost"
+              size="icon"
+              className={cn(
+                "h-8 w-8",
+                editor.isActive("strike") && "bg-accent text-accent-foreground"
+              )}
+            >
+              <StrikethroughIcon className="h-4 w-4" />
+            </AdminButton>
+            <AdminButton
+              onClick={() => editor.chain().focus().toggleHighlight().run()}
+              variant="ghost"
+              size="icon"
+              className={cn(
+                "h-8 w-8",
+                editor.isActive("highlight") &&
+                  "bg-accent text-accent-foreground"
+              )}
+            >
+              <span className="w-4 bg-yellow-200">H</span>
+            </AdminButton>
+            <AdminButton
+              onClick={() => editor.chain().focus().toggleUnderline().run()}
+              variant="ghost"
+              size="icon"
+              className={cn(
+                "h-8 w-8",
+                editor.isActive("underline") &&
+                  "bg-accent text-accent-foreground"
+              )}
+            >
+              <span className="w-4 underline">U</span>
+            </AdminButton>
+            <div className="h-5 w-px bg-gray-300" />
+            <AdminButton
+              onClick={() => editor.chain().focus().toggleBulletList().run()}
+              variant="ghost"
+              size="icon"
+              className={cn(
+                "h-8 w-8",
+                editor.isActive("bulletList") &&
+                  "bg-accent text-accent-foreground"
+              )}
+            >
+              <ListIcon className="h-4 w-4" />
+            </AdminButton>
+            <AdminButton
+              onClick={() => editor.chain().focus().toggleOrderedList().run()}
+              variant="ghost"
+              size="icon"
+              className={cn(
+                "h-8 w-8",
+                editor.isActive("orderedList") &&
+                  "bg-accent text-accent-foreground"
+              )}
+            >
+              <ListOrderedIcon className="h-4 w-4" />
+            </AdminButton>
+            <AdminButton
+              onClick={() => editor.chain().focus().toggleBlockquote().run()}
+              variant="ghost"
+              size="icon"
+              className={cn(
+                "h-8 w-8",
+                editor.isActive("blockquote") &&
+                  "bg-accent text-accent-foreground"
+              )}
+            >
+              <QuoteIcon className="h-4 w-4" />
+            </AdminButton>
+            <div className="h-5 w-px bg-gray-300" />
+            <AdminButton
+              onClick={() => {}}
+              variant="ghost"
+              size="icon"
+              className="h-8 w-8"
+            >
+              <ImageIcon className="h-4 w-4" />
+            </AdminButton>
+            <div className="h-5 w-px bg-gray-300" />
+            <AdminButton
+              onClick={() => editor.chain().focus().undo().run()}
+              disabled={!editor.can().undo()}
+              variant="ghost"
+              size="icon"
+              className="h-8 w-8"
+            >
+              <Undo2Icon className="h-4 w-4" />
+            </AdminButton>
+            <AdminButton
+              onClick={() => editor.chain().focus().redo().run()}
+              disabled={!editor.can().redo()}
+              variant="ghost"
+              size="icon"
+              className="h-8 w-8"
+            >
+              <Redo2Icon className="h-4 w-4" />
+            </AdminButton>
+          </div>
+          <div className="px-4 text-sm text-gray-500">
+            {(
+              editor.storage.characterCount.characters() as number
+            ).toLocaleString()}{" "}
+            / {limit.toLocaleString()}
+          </div>
+        </div>
+        <Separator />
       </div>
       <BubbleMenu
         editor={editor}
@@ -322,15 +339,7 @@ const AdminEditor = () => {
           </>
         )}
       </BubbleMenu>
-      <EditorContent className="mx-4 my-2" editor={editor} />
-      <div className="flex flex-row justify-end">
-        <span className="px-4 text-sm text-gray-500">
-          {(
-            editor.storage.characterCount.characters() as number
-          ).toLocaleString()}{" "}
-          / {limit.toLocaleString()}
-        </span>
-      </div>
+      <EditorContent className="px-4 py-2" editor={editor} />
     </div>
   );
 };
