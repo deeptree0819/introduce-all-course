fb489122a78b6e3cec09e5b4eee9a8031f2a3606
YG Kim <hoodie0512@gmail.com>
2024-08-08 00:25:22 +0900

    Chore: Enable introspectiveComments

--- introduce-all-course-api/src/admin/auth/auth.controller.ts
+++ introduce-all-course-api/src/admin/auth/auth.controller.ts
@@ -2,7 +2,6 @@ import { AuthService } from "@admin/auth/auth.service";
 import { CustomApiOperation } from "@common/decorators/api-operation.decorator";
 import { Public } from "@common/decorators/public.decorator";
 import { Body, Controller, Post, Req } from "@nestjs/common";
-import { ApiBody } from "@nestjs/swagger";
 import { LoginResultDto } from "./dtos/login-result.dto";
 import { LoginWithEmailDto } from "./dtos/login-with-email.dto";
 
@@ -10,17 +9,6 @@ import { LoginWithEmailDto } from "./dtos/login-with-email.dto";
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @ApiBody({
-    type: LoginWithEmailDto,
-    examples: {
-      admin: {
-        value: {
-          email: "admin@gmail.com",
-          password: "admin1234",
-        },
-      },
-    },
-  })
   @CustomApiOperation({
     summary: "관리자 이메일 로그인",
     tags: ["admin-auth"],
