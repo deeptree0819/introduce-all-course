b3767b447417c8395b4759ffebb50d9f73361792
YG Kim <hoodie0512@gmail.com>
2024-08-08 09:07:55 +0900

    Chore: Change variable name

--- codegen.config.js
+++ codegen.config.js
@@ -3,12 +3,12 @@ const path = require("path");
 dotenv.config({ path: "./introduce-all-course-api/.env" });
 
 const port = process.env.PORT || 8000;
-const frontOutPath = path.resolve(`introduce-all-course-web/app/generated/`);
+const outPath = path.resolve(`introduce-all-course-web/app/generated/`);
 
 module.exports = {
   enable: true,
   exportCore: true,
   exportService: true,
   url: `http://localhost:${port}/api-json`,
-  outPath: frontOutPath,
+  outPath: outPath,
 };
