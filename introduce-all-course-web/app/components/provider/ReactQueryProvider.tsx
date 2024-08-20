18b44fdf73d9ec78b6c4187879096aea502ebba3
YG Kim <hoodie0512@gmail.com>
2024-08-20 21:09:27 +0900

    Fix: Exclude login page for 401 redirect

--- introduce-all-course-web/app/components/provider/ReactQueryProvider.tsx
+++ introduce-all-course-web/app/components/provider/ReactQueryProvider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import {
+  MutationCache,
   QueryCache,
   QueryClient,
   QueryClientProvider,
@@ -17,11 +18,15 @@ interface ReactQueryProviderProps {
 
 const ReactQueryProvider = ({ children }: ReactQueryProviderProps) => {
   const { push } = useRouter();
+  const excludedPages = ["/admin/login"];
+
   const [queryClient] = React.useState(
     () =>
       new QueryClient({
         queryCache: new QueryCache({
           onError: (error) => {
+            if (excludedPages.includes(window.location.pathname)) return;
+
             if ("status" in (error as ApiError)) {
               if ((error as ApiError).status === 401) {
                 queryClient.clear();
@@ -32,6 +37,25 @@ const ReactQueryProvider = ({ children }: ReactQueryProviderProps) => {
             }
           },
         }),
+        mutationCache: new MutationCache({
+          onError: (error) => {
+            if (excludedPages.includes(window.location.pathname)) return;
+
+            if ("status" in (error as ApiError)) {
+              if ((error as ApiError).status === 401) {
+                queryClient.clear();
+                deleteCookie("token");
+                OpenAPI.TOKEN = "";
+                push("/");
+              }
+            }
+          },
+        }),
+        defaultOptions: {
+          queries: {
+            retry: false,
+          },
+        },
       })
   );
 
