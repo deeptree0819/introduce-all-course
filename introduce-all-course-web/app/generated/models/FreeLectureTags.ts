49b768fa6c2ea04bf1179c28b03f744220e557cc
YG Kim <hoodie0512@gmail.com>
2024-08-22 15:15:16 +0900

    Add: Code gen for free lecture apis

---
+++ introduce-all-course-web/app/generated/models/FreeLectureTags.ts
@@ -0,0 +1,8 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type FreeLectureTags = {
+  free_lecture_tags_id: number;
+  free_lecture_tag_name: string;
+};
