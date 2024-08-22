6b7da06dc52704fedbd015f033a135b636974724
YG Kim <hoodie0512@gmail.com>
2024-08-22 18:11:05 +0900

    Feat: Connect free lecture tag apis

--- introduce-all-course-web/app/admin/(auth)/free-lecture/tags/components/FreeLectureTagAddButton.tsx
+++ introduce-all-course-web/app/admin/(auth)/free-lecture/tags/components/FreeLectureTagAddButton.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Dialog,
   DialogContent,
@@ -7,9 +9,17 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@components/ui/dialog";
+import { Form, FormField, FormItem, FormMessage } from "@components/ui/form";
+import { CreateFreeLectureTagDto } from "@generated/index";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
+import { useCreateFreeLectureTag } from "@/app/hooks/admin/adminFreeLectureHooks";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+
+import { CreateFreeLectureTagSchema } from "../schema";
 
 type FreeLectureTagAddButtonProps = {
   className?: string;
@@ -18,10 +28,23 @@ type FreeLectureTagAddButtonProps = {
 const FreeLectureTagAddButton = ({
   className,
 }: FreeLectureTagAddButtonProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const form = useForm<CreateFreeLectureTagDto>({
+    mode: "onSubmit",
+    resolver: zodResolver(CreateFreeLectureTagSchema),
+  });
+  const { handleSubmit } = form;
+  const { mutate: createFreeLectureTag } = useCreateFreeLectureTag(() =>
+    setIsOpen(false)
+  );
+
   return (
-    <Dialog>
+    <Dialog open={isOpen}>
       <DialogTrigger asChild>
-        <Button className={className}>태그 추가</Button>
+        <Button className={className} onClick={() => setIsOpen(true)}>
+          태그 추가
+        </Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
@@ -30,12 +53,34 @@ const FreeLectureTagAddButton = ({
             추가하고 싶은 태그를 입력해주세요.
           </DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <Input id="name" placeholder="인공지능, 인턴 등" />
-        </div>
-        <DialogFooter>
-          <Button type="submit">저장하기</Button>
-        </DialogFooter>
+
+        <Form {...form}>
+          <form onSubmit={handleSubmit((data) => createFreeLectureTag(data))}>
+            <FormField
+              control={form.control}
+              name="free_lecture_tag_name"
+              render={({ field }) => (
+                <FormItem className="grid py-4">
+                  <Input {...field} id="name" placeholder="인공지능, 인턴 등" />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <DialogFooter className="flex flex-row space-x-3">
+              <Button
+                type="button"
+                variant="secondary"
+                onClick={() => setIsOpen(false)}
+                className="w-full"
+              >
+                취소
+              </Button>
+              <Button type="submit" className="w-full">
+                저장하기
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
