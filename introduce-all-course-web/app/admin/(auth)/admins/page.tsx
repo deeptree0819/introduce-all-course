76a3a61d5527fdfb5f03ec5bfb6d5b44b5f14496
YG Kim <hoodie0512@gmail.com>
2024-08-25 19:12:44 +0900

    Design: Adjust create admin button

--- introduce-all-course-web/app/admin/(auth)/admins/page.tsx
+++ introduce-all-course-web/app/admin/(auth)/admins/page.tsx
@@ -6,7 +6,7 @@ import AdminTable from "./components/AdminTable";
 
 const AdminUsersPage = () => {
   return (
-    <div className="flex flex-col space-y-10 p-5">
+    <div className="flex max-w-[1300px] flex-col space-y-10 p-5">
       <p className="text-3xl">어드민 정보 관리</p>
       <div className="flex flex-col space-y-3">
         <AdminTable />
