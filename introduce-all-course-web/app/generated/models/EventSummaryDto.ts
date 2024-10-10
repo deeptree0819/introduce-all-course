bbd5fb893c22eac64395bd2a59c6459585514d6f
YG Kim <hoodie0512@gmail.com>
2024-10-10 17:22:59 +0900

    Fix: Count d-day from when event starts

--- introduce-all-course-web/app/generated/models/EventSummaryDto.ts
+++ introduce-all-course-web/app/generated/models/EventSummaryDto.ts
@@ -6,6 +6,7 @@ export type EventSummaryDto = {
   events_id: number;
   event_title: string;
   event_organization: string;
+  event_start_at: string;
   event_end_at: string;
   event_thumbnail_url: string;
 };
